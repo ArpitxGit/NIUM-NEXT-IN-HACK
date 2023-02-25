7f6d2927a1a70b6a72f12a93d391a817cf330dd3
Chirag <56735482+Chirag018@users.noreply.github.com>
2023-02-25 18:39:01 +0530

    initial commit

---
+++ frontnd/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>
+      NIUM NEXT IN HACK
+    </h1>
+    </div>
+  );
+}
+
+export default App;
