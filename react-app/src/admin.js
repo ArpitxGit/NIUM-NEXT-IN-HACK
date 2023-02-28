6666d7251bc10d6cfaa05c60f8204d8bea1a33b0
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2023-02-28 23:55:23 +0530

    update

---
+++ react-app/src/admin.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./admin.css";
+function admin() {
+    return (
+      <div className="row">
+        <div className="col-md-3">
+            <div></div>
+            <p>This is the second page.</p>
+            <div></div>
+        </div>
+      </div>
+    );
+}
+
+export default admin;
