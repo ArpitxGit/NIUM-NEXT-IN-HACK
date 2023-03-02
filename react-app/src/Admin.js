285c9ce5ff6f685ced93b06505e9426ff1222a3a
Raja543 <rajakumarmahto952@gmail.com>
2023-03-02 23:04:23 +0530

    modifying this pages

--- react-app/src/Admin.js
+++ react-app/src/Admin.js
@@ -1,80 +1,83 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./admin.css";
 function Admin() {
-    return (
-      <div className="container">
-        <div className="row">
-            <div className="col-md-12">
-                <div></div>
-                    <Link to={"/"}><button id="home2">Home</button></Link>
-                    <p>Welcome Administrator</p>
+  const [matchName, setMatchName] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+  const [totalTickets, setTotalTickets] = useState(0);
 
-                    <button id="addmatch">Add Match</button>
-                    
-                <div></div>
-            </div>
+  const handleMatchNameChange = (event) => {
+    setMatchName(event.target.value);
+  };
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const handleTotalTicketsChange = (event) => {
+    setTotalTickets(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMatch = {
+      matchName: matchName,
+      location: location,
+      date: date,
+      totalTickets: totalTickets,
+    };
+    // Do something with the new match data
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="container1">
+        <div>
+          <label htmlFor="matchName">Match Name:</label>
+          <input
+            type="text"
+            id="matchName"
+            value={matchName}
+            onChange={handleMatchNameChange}
+          />
         </div>
+        <div>
+          <label htmlFor="location">Location:</label>
+          <input
+            type="text"
+            id="location"
+            value={location}
+            onChange={handleLocationChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="date">Date:</label>
+          <input
+            type="date"
+            id="date"
+            value={date}
+            onChange={handleDateChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="totalTickets">Total Tickets:</label>
+          <input
+            type="number"
+            id="totalTickets"
+            value={totalTickets}
+            onChange={handleTotalTicketsChange}
+          />
+        </div>
+        <button type="submit">Add</button>
       </div>
-    );
+    </form>
+  );
 }
+
 export default Admin;
-
-
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-
-// import "./admin.css";
-
-// function Admin() {
-//   const [name, setName] = useState("");
-//   const [location, setLocation] = useState("");
-//   const [date, setDate] = useState("");
-//   const [totalTickets, setTotalTickets] = useState(0);
-
-//   const handleNameChange = (event) => {
-//     setName(event.target.value);
-//   };
-
-//   const handleLocationChange = (event) => {
-//     setLocation(event.target.value);
-//   };
-
-//   const handleDateChange = (event) => {
-//     setDate(event.target.value);
-//   };
-
-//   const handleTotalTicketsChange = (event) => {
-//     setTotalTickets(event.target.value);
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // Do something with the form data
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit} className="ticket-form">
-//       <div className="form-group">
-//         <label htmlFor="name" className="form-label">Name: </label>
-//         <input type="text" id="name" value={name} onChange={handleNameChange} className="form-input" />
-//       </div>
-//       <div className="form-group">
-//         <label htmlFor="location" className="form-label">Location: </label>
-//         <input type="text" id="location" value={location} onChange={handleLocationChange} className="form-input" />
-//       </div>
-//       <div className="form-group">
-//         <label htmlFor="date" className="form-label">Date: </label>
-//         <input type="date" id="date" value={date} onChange={handleDateChange} className="form-input" />
-//       </div>
-//       <div className="form-group">
-//         <label htmlFor="totalTickets" className="form-label">Total Tickets: </label>
-//         <input type="number" id="totalTickets" value={totalTickets} onChange={handleTotalTicketsChange} className="form-input" />
-//       </div>
-//       <button type="submit" className="form-button">Add</button>
-//     </form>
-//   );
-// }
-// export default Admin;
